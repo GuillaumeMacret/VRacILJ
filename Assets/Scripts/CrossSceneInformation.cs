afd462c29f1eaf5edd32f1bda4ffbf8c37131ece
Guillaume Macret <guillaume.macret@gmail.com>
2020-02-05 10:45:32 +0100

    Rel1

--- Assets/Scripts/CrossSceneInformation.cs
+++
@@ -1,6 +0,0 @@
-﻿public static class CrossSceneInformation
-{
-    public static bool PlayerHasFinishedGame { get; set; } = false;
-    public static int PlayerFinalPositionInRace { get; set; }
-    public static string TimeToFinishRace { get; set; }
-}
--- References.md
+++
@@ -1,3 +0,0 @@
-# Used / Usefull Sources
-
-- **Car simple controls** : https://www.youtube.com/watch?v=j6_SMdWeGFI
